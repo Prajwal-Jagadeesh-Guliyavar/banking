ca5c0fdd657d275e80fbaa3be61440587086c95b
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2025-04-16 14:25:11 +0530

    updated the footer.js

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () =>{
+    return (
+        <footer style={{
+            backgroundColor: '#333',
+            color: '#fff',
+            textAlign: 'center',
+            padding: '10px',
+            position: 'fixed',
+            bottom: '0',
+            width: '100%'
+          }}>
+            Banking Management System © 2025
+        </footer>
+    );
+};
+
+export default Footer;
