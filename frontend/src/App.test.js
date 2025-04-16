0bf6e669a1098a09c4afba5a7fe166bb1f4a38ff
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2025-04-16 10:16:11 +0530

    app js,css and testing

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the dashboard link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const dashboardElement = screen.getByText(/dashboard/i);
+  expect(dashboardElement).toBeInTheDocument();
 });
