5f13edd3c2ec4c6067c0b4b5658ba3f5d603190b
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2025-04-19 00:56:07 +0530

    added the style sheets and configured the endpoints

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,14 +1,30 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL : 'http://localhost:5000/api',
+  baseURL: 'http://localhost:5000/api',
 });
 
-export const getCustomers = () => api.get('/customers');
-export const createCustomer = (data) => api.post('/customers', data);
+export const getCustomers = () => api.get('/customers/');
+export const createCustomer = (data) => api.post('/customers/', data);
+export const customerSignup = (data) => api.post('/auth/customer/signup', data);
+export const customerLogin = (data) => api.post('/auth/customer/login', data);
 
-export const getAccounts = () => api.get('/accounts');
-export const createAccount = (data) => api.post('/accounts', data);
+export const getAccounts = () => api.get('/accounts/');
+export const createAccount = (data) => api.post('/accounts/', data);
 
-export const getTransactions = () => api.get('/transactions');
-export const createTransaction = (data) => api.post('transactions', data);
+export const getTransactions = () => api.get('/transactions/');
+export const createTransaction = (data) => api.post('/transactions/', data);
+
+export const adminSignup = (data) => api.post('/auth/admin/signup', data);
+export const adminLogin = (data) => api.post('/auth/admin/login', data);
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default api;
