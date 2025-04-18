4a410b5cd99c04df9f2f310668515937fc22825c
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2025-04-18 13:41:33 +0530

    added a new entity for admins and email and password fields.

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Customer(db.Model):
     __tablename__ = 'customers'
@@ -10,10 +11,27 @@ class Customer(db.Model):
     lname = db.Column(db.String(50), nullable=False)
     address = db.Column(db.String(100))
     phone_no = db.Column(db.String(15))
+
+    email = db.Column(db.String(50), unique = True, nullable = False)
+    password = db.Column(db.String(100), nullable=False)
     
     #1 to many realtions
     accounts = db.relationship('Account', backref='owner', lazy=True)
     loans = db.relationship('Loan', backref='customer', lazy=True)
+
+class Administrator(db.Model):
+    __tablename__ = 'administrators'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+        
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
 class Account(db.Model):
     __tablename__ = 'accounts'
