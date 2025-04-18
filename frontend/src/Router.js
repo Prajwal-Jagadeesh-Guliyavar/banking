9cd872c55328dc290b12437337130405cc88d445
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2025-04-19 00:54:17 +0530

    added the authentication routes

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,9 +1,14 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import CustomersPage from "./pages/CustomersPage";
-import AccountsPage from "./pages/AccountsPage";
-import TransactionsPage from "./pages/TransactionsPage";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import CustomersPage from './pages/CustomersPage';
+import AccountsPage from './pages/AccountsPage';
+import TransactionsPage from './pages/TransactionsPage';
+
+import CustomerLogin from './pages/CustomerLogin';
+import CustomerSignup from './pages/CustomerSignup';
+import AdminLogin from './pages/AdminLogin';
+import AdminSignup from './pages/AdminSignup';
 
 const AppRouter = () => {
   return (
@@ -12,6 +17,11 @@ const AppRouter = () => {
       <Route path="/customers" element={<CustomersPage />} />
       <Route path="/accounts" element={<AccountsPage />} />
       <Route path="/transactions" element={<TransactionsPage />} />
+      {/* Authentication Routes */}
+      <Route path="/login" element={<CustomerLogin />} />
+      <Route path="/signup" element={<CustomerSignup />} />
+      <Route path="/admin/login" element={<AdminLogin />} />
+      <Route path="/admin/signup" element={<AdminSignup />} />
     </Routes>
   );
 };
