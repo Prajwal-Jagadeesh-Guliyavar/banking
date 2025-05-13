fdd0f56039d4edac30750d977325aed5627bfbb8
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2025-05-13 07:57:45 +0530

    entry point main.tsx is added with some of the cleanuo in the src and the vite-env

--- banking-system/frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders the dashboard link', () => {
-  render(<App />);
-  const dashboardElement = screen.getByText(/dashboard/i);
-  expect(dashboardElement).toBeInTheDocument();
-});
